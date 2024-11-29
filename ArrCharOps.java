2f49a0f7e62f147af0d979ef7de12ebbbe79260e
Roie Drukman <roie.drukman@post.runi.ac.il>
2024-11-29 17:15:56 +0200

    5

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -2,24 +2,25 @@
  *  The library also features a string comparison method. */
 public class ArrCharOps {
     public static void main(String[] args) {
-        //String str = "clearly";
+        String str = "clearly";
         char[] arr1 = {'c','l','e','a','r','l','y'};
         char[] arr2 = {'U','n','d','e','r','s','t', 'o', 'o', 'd'};
-        //System.out.println(str);  // Prints the string
-        //println(arr1);            // Prints an array of characters
-        //System.out.println(charAt(arr1,2));      
-        //System.out.println(indexOf(arr1,'l'));  
-        //System.out.println(indexOf(arr1,'l',3)); 
-        //System.out.println(lastIndexOf(arr1, 'l'));
-        //System.out.println(concat(arr1, arr2));
-        //System.out.println(subArray(arr2, 2, 9));
+        System.out.println(equals(arr1,arr2));
+        System.out.println(str);  // Prints the string
+        println(arr1);            // Prints an array of characters
+        System.out.println(charAt(arr1,2));      
+        System.out.println(indexOf(arr1,'l'));  
+        System.out.println(indexOf(arr1,'l',3)); 
+        System.out.println(lastIndexOf(arr1, 'l'));
+        System.out.println(concat(arr1, arr2));
+        System.out.println(subArray(arr2, 2, 9));
         System.out.println(compareTo("abcd", "abcd"));
         System.out.println(compareTo("abc", "abcd"));
         System.out.println(compareTo("abw", "abcd"));
         System.out.println(compareTo("Abcd", "a"));
         System.out.println(compareTo("apple", "banana"));
-        System.out.println(compareTo("apple", "applepie"));
-        System.out.println(compareTo("Zoo", "zoo"));
+        //System.out.println(compareTo("apple", "applepie"));
+        //System.out.println(compareTo("Zoo", "zoo"));
         System.out.println(hashCode(arr1));
         System.out.println(hashCode(arr2));
     }
@@ -44,12 +45,14 @@ public class ArrCharOps {
      */
     public static boolean equals(char[] arr1, char[] arr2) {
         if(arr1.length == 0 || arr2.length == 0)
-        return true;
-        for(int i = 0; i < arr1.length; i++) {
-            for(int j = 0; j < arr2.length; j++) {
-                if(arr1[1] != arr2[j])
+        return false;
+        if(arr1.length != arr2.length)
+        return false;
+        else if(arr1.length == arr2.length) {
+            for(int i = 0; i < arr1.length; i++) {
+                if(arr1[i] != arr2[i])
                 return false;
-            }
+                }
         }
         return true;
     }
@@ -90,6 +93,15 @@ public class ArrCharOps {
     */
     public static char[] concat(char[] arr1, char[] arr2) {
         char[] newArr = new char[arr1.length + arr2.length];
+        if(arr2.length == 0) {
+            for(int i = 0; i < newArr.length; i++) {
+                newArr[i] = arr1[i];
+            }
+        } else if(arr1.length == 0) {
+            for(int i = 0; i < newArr.length; i++) {
+                newArr[i] = arr2[i];
+            }
+        } else {
         for(int i = 0; i < arr1.length; i++) {
             newArr[i] = arr1[i];
         }
@@ -98,6 +110,7 @@ public class ArrCharOps {
             newArr[j] = arr2[i];
             i++;
         }
+    }
         return newArr;
     }
 
