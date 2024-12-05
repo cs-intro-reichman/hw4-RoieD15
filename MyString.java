88e44fe56c28368a0efd1347fe245c9f273c9169
Roie Drukman <roie.drukman@post.runi.ac.il>
2024-12-05 12:05:29 +0200

    f

--- MyString.java
+++ MyString.java
@@ -5,15 +5,14 @@ public class MyString {
         System.out.println("This costs 15 Sheksls : " + lowerCase("This costs 15 Sheksls"));
         System.out.println("TLV : " + lowerCase("TLV"));
         System.out.println("lowercase : " + lowerCase("lowercase"));
-
-        System.out.println("Testing contains:");
-        System.out.println(contains("unhappy", "happy")); // true
-        System.out.println(contains("happy", "unhappy")); // false
-        System.out.println(contains("historical", "story")); // false
-        System.out.println(contains("psychology", "psycho")); // true
-        System.out.println(contains("personality", "son")); // true
-        System.out.println(contains("personality", "dad")); // false
-        System.out.println(contains("resignation", "sign")); // true
+        //System.out.println("Testing contains:");
+        //System.out.println(contains("unhappy", "happy")); // true
+        //System.out.println(contains("happy", "unhappy")); // false
+        //System.out.println(contains("historical", "story")); // false
+        //System.out.println(contains("psychology", "psycho")); // true
+        //System.out.println(contains("personality", "son")); // true
+        //System.out.println(contains("personality", "dad")); // false
+        //System.out.println(contains("resignation", "sign")); // true
     }
 
     /** Returns the lowercase version of the given string. */
@@ -21,21 +20,30 @@ public class MyString {
         String newString = "";
         for(int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
-            newString += c + 32;
+            if(str.charAt(i) >= 65 && str.charAt(i) <= 90) {
+            c += 32;
+            newString += c;
+            } else {
+                newString += c;
+            }
         }
         return newString;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        for(int i = 0; i < str1.length(); i++) {
-            if(str1.substring(i) == str2)
-            return true;
+        if(str1.isEmpty() || str2.isEmpty() || str1.isEmpty() || str2.isEmpty()) {
+            return false;
         }
-        for(int i = 0; i < str1.length(); i++) {
-            if(str2.substring(i) == str1)
-            return true;
+        int minLength = Math.min(str1.length(), str2.length());
+        char[] newString = new char[minLength-1];
+        if(str1.length() > str2.length()) {
+            for(int i = 0; i < str2.length(); i++) {
+                newString[i] = str2.charAt(i);
+            }
+            if()
         }
-        return false;
+        
+        return true;
     }
 }
